0ac42585f992154e793b399d80b9d4f237339ccf
Salvatore Festa <salvatorefestatech@gmail.com>
2016-03-03 17:06:53 +0100

    Hazy Settings: Added Quick Pulldown and fix for SystemUI Tuner

--- src/com/android/settings/HazySettings.java
+++ src/com/android/settings/HazySettings.java
@@ -39,6 +39,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
@@ -57,12 +58,15 @@ public class HazySettings extends SettingsPreferenceFragment implements
     private static final String TAG = "HazySettings";
 
     private static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+    private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
     private static final String SWITCH_LAST_APP = "switch_last_app";
     private static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
     private WarnedListPreference mFontSizePref;
 
     private final Configuration mCurConfig = new Configuration();
+
+    private ListPreference mQuickPulldown;
 
     private SwitchPreference mTapToSleepPreference;
     private SwitchPreference mSwitchLastApp;
@@ -84,6 +88,15 @@ public class HazySettings extends SettingsPreferenceFragment implements
         mTapToSleepPreference = (SwitchPreference) findPreference(DOUBLE_TAP_SLEEP_GESTURE);
         mSwitchLastApp = (SwitchPreference) findPreference(SWITCH_LAST_APP);
         mVolBtnMusicControls = (SwitchPreference) findPreference(VOLBTN_MUSIC_CONTROLS);
+
+        // Quick pulldown
+        mQuickPulldown = (ListPreference) findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
+        int quickPulldown = Settings.System.getInt(resolver,
+            Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0);
+        mQuickPulldown.setValue(String.valueOf(quickPulldown));
+        mQuickPulldown.setSummary(mQuickPulldown.getEntry());
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -93,6 +106,8 @@ public class HazySettings extends SettingsPreferenceFragment implements
     }
 
     private void updateState() {
+        final ContentResolver resolver = getActivity().getContentResolver();
+
         // Update tap to sleep.
         if (mTapToSleepPreference != null) {
             mTapToSleepPreference.setChecked(Settings.System.getInt(getContentResolver(),
@@ -112,7 +127,7 @@ public class HazySettings extends SettingsPreferenceFragment implements
             mVolBtnMusicControls.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.VOLBTN_MUSIC_CONTROLS, 0) == 1);
             mVolBtnMusicControls.setOnPreferenceChangeListener(this);
-        }
+        }       
     }
 
     @Override
@@ -122,6 +137,7 @@ public class HazySettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         final String key = preference.getKey();
         if (preference == mTapToSleepPreference) {
             boolean value = (Boolean) objValue;
@@ -134,6 +150,13 @@ public class HazySettings extends SettingsPreferenceFragment implements
         if (preference == mVolBtnMusicControls) {
             boolean value = (Boolean) objValue;
             Settings.System.putInt(getContentResolver(), VOLBTN_MUSIC_CONTROLS, value ? 1 : 0);
+        }
+        if (preference == mQuickPulldown) {
+            int quickPulldown = Integer.valueOf((String) objValue);
+            int index = mQuickPulldown.findIndexOfValue((String) objValue);
+            Settings.System.putInt(resolver,
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, quickPulldown);
+            mQuickPulldown.setSummary(mQuickPulldown.getEntries()[index]);
         }
         return true;
     }
